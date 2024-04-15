5af6872fce0ac4f7ecbce0e7f6e3f2f960a80484
nuwang <2070605+nuwang@users.noreply.github.com>
2024-04-15 14:32:45 +0530

    Temporarily disable tests

--- pre-commit.bash
+++ pre-commit.bash
@@ -7,18 +7,18 @@ sed -i -e 's/insert_mod_if_required.*return 1/#&/' /usr/share/openvswitch/script
 
 # Add auth info for nova-compute
 # https://bugs.launchpad.net/devstack/+bug/1996465
-cat << EOF | tee -a /etc/nova/nova-cpu.conf
-[placement]
-region_name = RegionOne
-project_domain_name = Default
-project_name = service
-user_domain_name = Default
-password = password
-username = placement
-auth_url = http://172.28.0.2/identity
-auth_type = password
-EOF
+# cat << EOF | tee -a /etc/nova/nova-cpu.conf
+# [placement]
+# region_name = RegionOne
+# project_domain_name = Default
+# project_name = service
+# user_domain_name = Default
+# password = password
+# username = placement
+# auth_url = http://172.28.0.2/identity
+# auth_type = password
+# EOF
 
-# Disable namespace in libvirt/qemu.conf
-# https://listman.redhat.com/archives/libvirt-users/2017-February/009734.html
-echo 'namespaces = []' >> /etc/libvirt/qemu.conf
+# # Disable namespace in libvirt/qemu.conf
+# # https://listman.redhat.com/archives/libvirt-users/2017-February/009734.html
+# echo 'namespaces = []' >> /etc/libvirt/qemu.conf
